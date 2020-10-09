d63387f6d02cdc2c4504e5c1e530e00b3cd235bb
Larry Scott <larry2education@gmail.com>
2020-10-08 19:50:40 -0500

    Passed All Tests

--- copyspecial.py
+++ copyspecial.py
@@ -7,7 +7,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 # give credits
-__author__ = "???"
+__author__ = "Larry Scott with help from John"
 
 import re
 import os
@@ -19,17 +19,28 @@ import argparse
 
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
-    # your code here
-    return
+    special_list = []
+    the_paths = os.listdir(dirname)
+    for name in the_paths:
+        same = re.search(r'__(\w+)__', name)
+        if same:
+            special_list.append(os.path.abspath(os.path.join(dirname, name)))
+    return special_list
 
 
 def copy_to(path_list, dest_dir):
-    # your code here
+    """Copy the files to the directory."""
+    if not os.path.isdir(dest_dir):
+        os.makedirs(dest_dir)
+    for path in path_list:
+        shutil.copy(path, dest_dir)
     return
 
 
 def zip_to(path_list, dest_zip):
-    # your code here
+    """Zip the files to a new zip file."""
+    for path in path_list:
+        subprocess.run(['zip', "-j", dest_zip, path])
     return
 
 
@@ -40,11 +51,21 @@ def main(args):
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
     # TODO: add one more argument definition to parse the 'from_dir' argument
+    parser.add_argument('from_dir', help='target dir for special files')
     ns = parser.parse_args(args)
 
+    special_paths = get_special_paths(ns.from_dir)
     # TODO: you must write your own code to get the command line args.
     # Read the docs and examples for the argparse module about how to do this.
-
+    if not sys.argv:
+        parser.print_usage()
+    if ns.todir:
+        copy_to(special_paths, ns.todir)
+    elif ns.tozip:
+        zip_to(special_paths, ns.tozip)
+    else:
+        for path in special_paths:
+            print(path)
     # Parsing command line arguments is a must-have skill.
     # This is input data validation. If something is wrong (or missing) with
     # any required args, the general rule is to print a usage message and
